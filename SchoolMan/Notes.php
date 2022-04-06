d9624a617647687ced1ac7dd3e70ad1e23d816ef
Creepieable <38226503+Creepieable@users.noreply.github.com>
2022-04-06 19:25:12 +0200

    Initial commit

---
+++ SchoolMan/Notes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Main</title>
+    <link type="image/png" sizes="16x16" rel="icon" href="./ico.png">
+        
+    <link rel="stylesheet" href="./lib/bootstrap-5.1.3/css/bootstrap.min.css">
+
+    <script src="./lib/JQuery/js/jquery-3.6.0.min.js"></script>
+    <script src="./lib/bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <?php include "./header.html" ?>
+
+
+</body>
+</html>
