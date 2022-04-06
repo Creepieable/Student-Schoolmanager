341ee2f78ff10decee7a734882ce2e26bfa1326c
Creepieable <38226503+Creepieable@users.noreply.github.com>
2022-04-06 19:33:49 +0200

    renaming files to english

---
+++ SchoolMan/calendar.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Main</title>
+    <link type="image/png" sizes="16x16" rel="icon" href="./ico.png">
+        
+    <link rel="stylesheet" href="./lib/bootstrap-5.1.3/css/bootstrap.min.css">
+
+    <script src="./lib/JQuery/js/jquery-3.6.0.min.js"></script>
+    <script src="./lib/bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <?php include "./header.html" ?>
+
+    <h4 class="mt-3 ms-2">2022</h4>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Datum</th>
+          <th scope="col">Tag</th>
+          <th scope="col">Aufgaben</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">05.04.</th>
+          <td>Dienstag</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">06.04.</th>
+          <td>Mitwoch</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">07.04.</th>
+          <td>Donnerstag</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">08.04.</th>
+          <td>Freitag</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">09.04.</th>
+          <td>Samstag</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+</body>
+</html>
