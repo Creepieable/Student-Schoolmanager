be1e952ad4f53dacb865ae5fac06d5d84fdde456
Creepieable <38226503+Creepieable@users.noreply.github.com>
2022-04-15 17:39:20 +0200

    Added calendar functionality
    Calendar now automatically displays current month and is moveable through months

--- SchoolMan/calendar.php
+++ SchoolMan/calendar.php
@@ -10,6 +10,14 @@
 
     <script src="./lib/JQuery/js/jquery-3.6.0.min.js"></script>
     <script src="./lib/bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+
+    <script src="./js/calendar.js"></script>
+
+    <style>
+    .cal-btn {
+        min-width: 3em;
+    }
+    </style>
 </head>
 
 <body>
@@ -39,48 +47,26 @@
 <div class=container-md>
   <div class="row">
     <div class="col-sm-8">
-      <h4 class="mt-3 ms-2">2022</h4>
+      <h4 class="mt-3 ms-2" id="table-h">2022</h4>
       <table class="table">
-        <thead>
+        <thead id='calendart-head'>
           <tr>
             <th scope="col">Datum</th>
             <th scope="col">Tag</th>
             <th scope="col">Aufgaben</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th scope="row">05.04.</th>
-            <td>Dienstag</td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row">06.04.</th>
-            <td>Mitwoch</td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row">07.04.</th>
-            <td>Donnerstag</td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row">08.04.</th>
-            <td>Freitag</td>
-            <td></td>
-          </tr>
-          <tr class="bg-light">
-            <th scope="row">09.04.</th>
-            <td>Samstag</td>
-            <td></td>
-          </tr>
-          <tr class="bg-light">
-            <th scope="row">10.04.</th>
-            <td>Sonntag</td>
-            <td></td>
-          </tr>
+        <tbody id='calendart-body'>
+          <!-- filled by JS -->
         </tbody>
       </table>
+
+      <div class="text-center">
+        <div class="btn-group" role="group">
+          <button id="prev-btn" type="button" class="btn btn-primary cal-btn">Left</button>
+          <button id="next-btn" type="button" class="btn btn-primary cal-btn">Right</button>
+        </div>
+      </div>
     </div>
     <div class="col-sm-4">
       
---
+++ SchoolMan/js/calendar.js
@@ -0,0 +1,87 @@
+const monthName = ["Januar","Februar","März","April","Mai","Juni","Juli","August","September","October","November","Dezember"];
+const weekdayName = ["Sonntag", "Montag", "Dienstag", "Mitwoch", "Donnerstag", "Freitag", "Samstag"];
+let currentDate = new Date();
+
+$( document ).ready(function() {  
+    updateCalendar();
+});
+
+$(document).on("click",".cal-btn", function () {
+    var clickedBtnID = $(this).attr('id');
+    nextPrevDate(clickedBtnID);
+ });
+
+function updateCalendar(){
+    setTableH(currentDate);
+    setButtonNames(currentDate);  
+    buildTable();
+}
+
+function buildTable(){
+    let $cal = $('#calendart-body');
+    let daysCount = daysInMonth(currentDate.getMonth(), currentDate.getFullYear());
+
+    $cal.empty();
+
+    for (let i = 0; i < daysCount; i++) {
+        let d = new Date(currentDate.getFullYear(), currentDate.getMonth(), i+1);
+        $cal.append(buildTableRow(d));
+    }
+}
+
+function buildTableRow (date){
+    let $row = $('<tr></tr>');
+    let $date = $('<th></th>');
+    let $day = $('<td></td>');
+    let $tasks = $('<td></td>');
+
+    let today = new Date();
+    if(date.getDate() == today.getDate() && date.getMonth() == today.getMonth() && date.getFullYear() == today.getFullYear()){
+        $row.addClass('table-primary');
+    }
+    else{
+        if(date.getDay() == 0 || date.getDay() == 6){
+            $row.addClass('bg-light');
+            $row.addClass('text-muted');
+        }
+        else{
+            $row.addClass('bg-light');
+        }
+    }
+    
+    
+
+    $date.attr('scope', 'row').text(date.getDate() + '. ' + (date.getMonth()+1) + '.');
+    $day.text(weekdayName[date.getDay()]);
+
+    $row.append($date).append($day).append($tasks);
+    return $row;
+}
+
+function nextPrevDate(btn){
+    if(btn == 'next-btn'){
+        currentDate = new Date(currentDate.setMonth(currentDate.getMonth()+1));    
+    }
+    if(btn == 'prev-btn'){
+        currentDate = new Date(currentDate.setMonth(currentDate.getMonth()-1));
+
+        console.log(currentDate);
+    }
+
+    updateCalendar();
+}
+
+function setTableH(date){
+    $('#table-h').text(monthName[date.getMonth()] + ' - ' + date.getFullYear());
+}
+
+function setButtonNames(date){
+    $('#prev-btn').text(monthName[new Date(date.getFullYear(), date.getMonth()-1).getMonth()]);
+    $('#next-btn').text(monthName[new Date(date.getFullYear(), date.getMonth()+1).getMonth()]);
+}
+
+// helper functions
+
+function daysInMonth (month, year) {
+    return new Date(year, month+1, 0).getDate();
+}
