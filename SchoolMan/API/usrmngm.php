6b7d445f1cd05e88720783f274ecd48d128cfcf1
Creepieable <38226503+Creepieable@users.noreply.github.com>
2022-05-11 10:02:22 +0200

    login funtionality
    added login functionality

--- SchoolMan/API/usrmngm.php
+++ SchoolMan/API/usrmngm.php
@@ -11,11 +11,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $data = json_decode($json);
         register($data);
     }
+    else if($_SERVER['HTTP_USR_MGM_TYPE'] === 'salt'){
+        header('Content-Type: application/json; charset=utf-8');
+        $json = trim(file_get_contents("php://input"));
+        $data = json_decode($json);
+        gequestSalt($data);
+    }
     else if($_SERVER['HTTP_USR_MGM_TYPE'] === 'login'){
         //header('Content-Type: application/json; charset=utf-8');
         $json = trim(file_get_contents("php://input"));
         $data = json_decode($json);
         login($data);
+    }
+    else if($_SERVER['HTTP_USR_MGM_TYPE'] === 'token'){
+        //header('Content-Type: application/json; charset=utf-8');
+        $json = trim(file_get_contents("php://input"));
+        $data = json_decode($json);
+        tokenAvail($data);
     }
     else{
         //echo "Error 400: bad request";
@@ -32,6 +44,7 @@ function register($POSTjson){
     $db = new mysqli($dbHost, $dbUsr, $dbPw, $dbName);
     if ($db -> connect_errno) {
         echo "Failed to connect to MySQL: " . $db -> connect_error;
+        http_response_code(500);
         exit();
     }
     $stmt = $db->prepare('INSERT INTO users (name, email, passwd, passwd)
@@ -40,7 +53,7 @@ function register($POSTjson){
     $email = strval($POSTjson->data->email);
     $password = strval($POSTjson->data->saltedPasswordHash);
     $salt = strval($POSTjson->data->salt);
-    $stmt->bind_param("ssss", $username, $email, $password, $salt);
+    $stmt->bind_param('ssss', $username, $email, $password, $salt);
 
     //$stmt->execute();
 
@@ -53,6 +66,135 @@ function register($POSTjson){
 }
 
 function login($POSTjson){
+    global $dbHost, $dbUsr, $dbPw, $dbName;
+    $db = new mysqli($dbHost, $dbUsr, $dbPw, $dbName);
+    if ($db -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $db -> connect_error;
+        http_response_code(500);
+        exit();
+    }
+    $stmt = $db->prepare('SELECT userID FROM users WHERE (name=? OR email=?) AND passwd=?;');
+    $user = strval($POSTjson->data->user);
+    $passwd = strval($POSTjson->data->password);
 
+    $stmt->bind_param('sss', $user, $user, $passwd);
+
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $userID = $result[0]["userID"];
+
+    $usersFound = count($result);
+    if($usersFound == 1){
+        $regObj = new \stdClass();
+        $regObj->type = 'login';
+        $regObj->status = 'done';
+        
+        $token = bin2hex(random_bytes(32));
+        tokenToDB($userID, $token);
+
+        $data = new \stdClass();
+        $data->user = $user;
+        $data->token = $token;
+
+        $regObj->data = $data;
+
+        $respJSON = json_encode($regObj);
+        echo $respJSON;
+        exit();
+    }
+    if($$usersFound == 0){
+        http_response_code(404); exit();   
+    }
+    else{
+        http_response_code(500); exit();
+    }
+}
+
+function tokenToDB($userID, $token){
+    global $dbHost, $dbUsr, $dbPw, $dbName;
+    $db = new mysqli($dbHost, $dbUsr, $dbPw, $dbName);
+    if ($db -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $db -> connect_error;
+        http_response_code(500);
+        exit();
+    }
+    $stmt = $db->prepare('INSERT INTO logintokens (token, userID) VALUES (?, ?); ');
+    $stmt->bind_param('si', $token, $userID);
+
+    $stmt->execute();
+}
+
+function gequestSalt($POSTjson){
+    global $dbHost, $dbUsr, $dbPw, $dbName;
+    $db = new mysqli($dbHost, $dbUsr, $dbPw, $dbName);
+    if ($db -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $db -> connect_error;
+        http_response_code(500);
+        exit();
+    }
+    $stmt = $db->prepare('SELECT salt FROM users WHERE name=? OR email=?;');
+    $user = strval($POSTjson->data->name);
+    $stmt->bind_param('ss', $user, $user);
+
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+    $usersFound = count($result);
+    if($usersFound == 1){
+        $regObj = new \stdClass();
+        $regObj->type = 'salt';
+        $regObj->status = 'done';
+        
+        $data = new \stdClass();
+        $data->salt = $result[0]["salt"];
+
+        $regObj->data = $data;
+
+        $respJSON = json_encode($regObj);
+        echo $respJSON;
+        exit();
+    }
+    if($$usersFound == 0){
+        http_response_code(404); exit();   
+    }
+    else{
+        http_response_code(500); exit();
+    }
+}
+
+function tokenAvail($POSTjson){
+    global $dbHost, $dbUsr, $dbPw, $dbName;
+    $db = new mysqli($dbHost, $dbUsr, $dbPw, $dbName);
+    if ($db -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $db -> connect_error;
+        http_response_code(500);
+        exit();
+    }
+    $stmt = $db->prepare('SELECT userID FROM logintokens WHERE token=?;');
+    $userToken = strval($POSTjson->data->token);
+
+    $stmt->bind_param('s', $userToken);
+
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $userID = $result[0]["userID"];
+
+    $usersFound = count($result);
+
+    if($usersFound == 1){
+        $regObj = new \stdClass();
+        $regObj->type = 'token';
+        $regObj->status = 'avail';
+
+        $respJSON = json_encode($regObj);
+        echo $respJSON;
+        exit();
+    }
+    if($$usersFound == 0){
+        http_response_code(404); exit();
+    }
+    else{
+        http_response_code(500); exit();
+    }
 }
 ?>
--- SchoolMan/js/login.js
+++ SchoolMan/js/login.js
@@ -1,13 +1,107 @@
 if(Cookies.get('__scman_us_t') !== undefined){
-    window.location.href = "./calendar.php";
+    let token = Cookies.get('__scman_us_t');
+    
+    let postJSON = {
+        request:"POST",
+        type:"token",
+        data:{
+            token: token
+        }
+    }
+
+    $.ajax({
+        url: "./API/usrmngm.php",
+        type: 'POST',
+        headers: {"usr-mgm-type":"token"},
+        data: JSON.stringify(postJSON),
+        //dataType: "json",
+        success: function(text) {
+            console.log(text);
+            
+            if(text.data.status === "avail"){
+                window.location.href = "./calendar.php";
+            }
+            else{
+                Cookies.remove('__scman_us_t');
+            }
+        },
+        error: function(xhr, status, error){
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+            console.log('Error - ' + errorMessage);
+            console.log(error);
+        }
+    });
 }
 
 $(document).on('click','#submit-btn', function () {  
-    if($('#keep-check').prop('checked')){
-        Cookies.set('__scman_us_t', 'gsdzufzasdfzufnklsdnkdklasdklgnasdfgnjasdfn', { expires: 60 });
-    }
-    else{
-        Cookies.set('__scman_us_t', 'gsdzufzasdfzufnklsdnkdklasdklgnasdfgnjasdfn');
-    }
-    window.location.href = "./calendar.php";
+    login();
 });
+
+
+function login(){
+    let name = $('#name-inp').val();
+    let pw = $('#pw-inp').val();
+
+    let postJSON = {
+        request:"POST",
+        type:"salt",
+        data:{
+            name: name
+        }
+    }
+
+    $.ajax({
+        url: "./API/usrmngm.php",
+        type: 'POST',
+        headers: {"usr-mgm-type":"salt"},
+        data: JSON.stringify(postJSON),
+        dataType: "json",
+        success: function(text) {
+            let salt = text.data.salt;
+
+            var md = forge.md.sha256.create();  
+            md.start();  
+            md.update(salt+pw, "utf8");  
+            let saltedPw = md.digest().toHex();
+
+            let postJSON = {
+                request:"GET",
+                type:"login",
+                data:{
+                    user: name,
+                    password: saltedPw
+                }
+            }
+
+            $.ajax({
+                url: "./API/usrmngm.php",
+                type: 'POST',
+                headers: {"usr-mgm-type":"login"},
+                data: JSON.stringify(postJSON),
+                dataType: "json",
+                success: function(text) {
+                    let token = text.data.token;
+
+                    if($('#keep-check').prop('checked')){
+                        Cookies.set('__scman_us_t', token, { expires: 60 });
+                        window.location.href = "./calendar.php";
+                    }
+                    else{
+                        Cookies.set('__scman_us_t', token);
+                        window.location.href = "./calendar.php";
+                    }        
+                },
+                error: function(xhr, status, error){
+                    var errorMessage = xhr.status + ': ' + xhr.statusText
+                    console.log('Error - ' + errorMessage);
+                    console.log(error);
+                }
+            });
+        },
+        error: function(xhr, status, error){
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+            console.log('Error - ' + errorMessage);
+            console.log(error);
+        }
+    });
+}
