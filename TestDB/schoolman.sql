1976ddbe26da069c83108d49bf0e77bc0e36cf6a
Creepieable <38226503+Creepieable@users.noreply.github.com>
2022-05-28 00:26:44 +0200

    Update schoolman.sql
    updated test Database

--- TestDB/schoolman.sql
+++ TestDB/schoolman.sql
@@ -13,7 +13,7 @@
     <link rel="stylesheet" type="text/css" href="js/vendor/codemirror/lib/codemirror.css?v=5.1.3">
     <link rel="stylesheet" type="text/css" href="js/vendor/codemirror/addon/hint/show-hint.css?v=5.1.3">
     <link rel="stylesheet" type="text/css" href="js/vendor/codemirror/addon/lint/lint.css?v=5.1.3">
-    <link rel="stylesheet" type="text/css" href="./themes/pmahomme/css/theme.css?v=5.1.3&nocache=1115175494ltr&server=1">
+    <link rel="stylesheet" type="text/css" href="./themes/pmahomme/css/theme.css?v=5.1.3&nocache=3242450449ltr&server=1">
     <link rel="stylesheet" type="text/css" href="./themes/pmahomme/css/printview.css?v=5.1.3" media="print" id="printcss">
     <title>localhost / 127.0.0.1 | phpMyAdmin 5.1.3</title>
     <script data-cfasync="false" type="text/javascript" src="js/vendor/jquery/jquery.min.js?v=5.1.3"></script>
@@ -56,7 +56,7 @@
 
 <script data-cfasync="false" type="text/javascript">
 // <![CDATA[
-CommonParams.setAll({common_query:"",opendb_url:"index.php?route=/database/structure",lang:"de",server:"1",table:"",db:"",token:"45613b643a777c502f5c68416c7e5446",text_dir:"ltr",show_databases_navigation_as_tree:true,pma_text_default_tab:"Anzeigen",pma_text_left_default_tab:"Struktur",pma_text_left_default_tab2:false,LimitChars:"50",pftext:"P",confirm:true,LoginCookieValidity:"1440",session_gc_maxlifetime:"1440",logged_in:true,is_https:false,rootPath:"/phpmyadmin/",arg_separator:"&",PMA_VERSION:"5.1.3",auth_type:"config",user:"root"});
+CommonParams.setAll({common_query:"",opendb_url:"index.php?route=/database/structure",lang:"de",server:"1",table:"",db:"",token:"6d44445347343451574d7d3a67585e5f",text_dir:"ltr",show_databases_navigation_as_tree:true,pma_text_default_tab:"Anzeigen",pma_text_left_default_tab:"Struktur",pma_text_left_default_tab2:false,LimitChars:"50",pftext:"",confirm:true,LoginCookieValidity:"1440",session_gc_maxlifetime:"1440",logged_in:true,is_https:false,rootPath:"/phpmyadmin/",arg_separator:"&",PMA_VERSION:"5.1.3",auth_type:"config",user:"root"});
 var firstDayOfCalendar = '0';
 var themeImagePath = '.\/themes\/pmahomme\/img\/';
 var mysqlDocTemplate = '.\/url.php\u003Furl\u003Dhttps\u00253A\u00252F\u00252Fdev.mysql.com\u00252Fdoc\u00252Frefman\u00252F8.0\u00252Fen\u00252F\u002525s.html';
@@ -284,7 +284,7 @@ $(function() {
                   <div id="pma_navigation_settings"><div class="page_settings"><form method="post" action="index.php&#x3F;route&#x3D;&#x25;2Fexport&amp;server&#x3D;1" class="config-form disableAjax">
   <input type="hidden" name="tab_hash" value="">
       <input type="hidden" name="check_page_refresh" id="check_page_refresh" value="">
-    <input type="hidden" name="token" value="45613b643a777c502f5c68416c7e5446">
+    <input type="hidden" name="token" value="6d44445347343451574d7d3a67585e5f">
   <input type="hidden" name="submit_save" value="Navi">
 <ul class="tabs responsivetable row">
       <li><a href="#Navi_panel">Navigationspanel</a></li>
@@ -561,159 +561,7 @@ $.extend(defaultValues, {
                     <span class="hide" id="instructions-1">
                         Drücken Sie Enter, um die Abfrage auszuführen                    </span>
                 </div>
-                                                            <div class="message history collapsed hide select"
-                            targetdb="schoolman" targettable="logintokens">
-                            <div class="action_content">
-                    <span class="action collapse">
-            Zuklappen
-                    </span>
-                            <span class="action expand">
-            Aufklappen
-                    </span>
-                            <span class="action requery">
-            Erneut abfragen
-                    </span>
-                            <span class="action edit">
-            Bearbeiten
-                    </span>
-                            <span class="action explain">
-            Erklären
-                    </span>
-                            <span class="action profiling">
-            Messen
-                    </span>
-                            <span class="action bookmark">
-            Lesezeichen
-                    </span>
-                            <span class="text failed">
-            Abfrage fehlgeschlagen
-                    </span>
-                            <span class="text targetdb">
-            Datenbank
-                            : <span>schoolman</span>
-                    </span>
-                            <span class="text query_time">
-            Abfragezeit
-                            : <span>Während der aktuellen Sitzung</span>
-                    </span>
-            </div>
-                            <span class="query">SELECT * FROM `logintokens`</span>
-                        </div>
-                                            <div class="message history collapsed hide select"
-                            targetdb="schoolman" targettable="logintokens">
-                            <div class="action_content">
-                    <span class="action collapse">
-            Zuklappen
-                    </span>
-                            <span class="action expand">
-            Aufklappen
-                    </span>
-                            <span class="action requery">
-            Erneut abfragen
-                    </span>
-                            <span class="action edit">
-            Bearbeiten
-                    </span>
-                            <span class="action explain">
-            Erklären
-                    </span>
-                            <span class="action profiling">
-            Messen
-                    </span>
-                            <span class="action bookmark">
-            Lesezeichen
-                    </span>
-                            <span class="text failed">
-            Abfrage fehlgeschlagen
-                    </span>
-                            <span class="text targetdb">
-            Datenbank
-                            : <span>schoolman</span>
-                    </span>
-                            <span class="text query_time">
-            Abfragezeit
-                            : <span>Während der aktuellen Sitzung</span>
-                    </span>
-            </div>
-                            <span class="query">SELECT * FROM `logintokens`</span>
-                        </div>
-                                            <div class="message history collapsed hide select"
-                            targetdb="schoolman" targettable="schedule">
-                            <div class="action_content">
-                    <span class="action collapse">
-            Zuklappen
-                    </span>
-                            <span class="action expand">
-            Aufklappen
-                    </span>
-                            <span class="action requery">
-            Erneut abfragen
-                    </span>
-                            <span class="action edit">
-            Bearbeiten
-                    </span>
-                            <span class="action explain">
-            Erklären
-                    </span>
-                            <span class="action profiling">
-            Messen
-                    </span>
-                            <span class="action bookmark">
-            Lesezeichen
-                    </span>
-                            <span class="text failed">
-            Abfrage fehlgeschlagen
-                    </span>
-                            <span class="text targetdb">
-            Datenbank
-                            : <span>schoolman</span>
-                    </span>
-                            <span class="text query_time">
-            Abfragezeit
-                            : <span>Während der aktuellen Sitzung</span>
-                    </span>
-            </div>
-                            <span class="query">SELECT * FROM `schedule`</span>
-                        </div>
-                                            <div class="message history collapsed hide select"
-                            targetdb="schoolman" targettable="tasks">
-                            <div class="action_content">
-                    <span class="action collapse">
-            Zuklappen
-                    </span>
-                            <span class="action expand">
-            Aufklappen
-                    </span>
-                            <span class="action requery">
-            Erneut abfragen
-                    </span>
-                            <span class="action edit">
-            Bearbeiten
-                    </span>
-                            <span class="action explain">
-            Erklären
-                    </span>
-                            <span class="action profiling">
-            Messen
-                    </span>
-                            <span class="action bookmark">
-            Lesezeichen
-                    </span>
-                            <span class="text failed">
-            Abfrage fehlgeschlagen
-                    </span>
-                            <span class="text targetdb">
-            Datenbank
-                            : <span>schoolman</span>
-                    </span>
-                            <span class="text query_time">
-            Abfragezeit
-                            : <span>Während der aktuellen Sitzung</span>
-                    </span>
-            </div>
-                            <span class="query">SELECT * FROM `tasks`</span>
-                        </div>
-                                                </div><!-- console_message_container -->
+                            </div><!-- console_message_container -->
             <div class="query_input">
                 <span class="console_query_input"></span>
             </div>
